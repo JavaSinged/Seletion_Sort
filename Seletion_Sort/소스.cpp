f9e8154e1ddb6cd1a3383cf91b7cd4e6d595ab2f
JavaSinged <shinjiwoong1656@gmail.com>
2018-04-12 09:44:16 +0900

    Selection_Sort 2

--- Seletion_Sort/소스.cpp
+++ Seletion_Sort/소스.cpp
@@ -13,14 +13,32 @@ void Selection_Sort(int a[], int n) {
 				index = j;
 			}
 		}
+
 		a[index] = a[i];
 		a[i] = min;
 
 		//step 별로 출력
 		printf("step %d : ", i);
-		for (int k = 0; k < n; k++) {
+		for (int k = 0; k < n; k++)
 			printf("%d \t", a[k]);
-			printf("\n");
-		}
+		printf("\n");
 	}
 }
+
+void main() {
+	int array[] = { 5,2,8,1,9,7,3,11,6 };
+	int i, count;
+	count = sizeof(array) / sizeof(int);
+
+	printf(" 정렬 전 데이터\n ");
+	for (i = 0; i < count; i++)
+		printf("%d \t", array[i]);
+	printf("\n");
+
+	Selection_Sort(array, count);
+		printf("정렬 후 데이터\n");
+		for (i = 0; i < count; i++)
+			printf("%d \t", array[i]);
+		printf("\n");
+}
+
